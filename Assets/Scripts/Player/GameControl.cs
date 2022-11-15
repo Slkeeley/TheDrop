76689c62bbd0acfbcbdd43a68e0fbc26d454be8f
Samuel Keeley <83038330+Slkeeley@users.noreply.github.com>
2022-11-14 21:49:07 -0800

    Level Unlocks

--- Assets/Scripts/Player/GameControl.cs
+++ Assets/Scripts/Player/GameControl.cs
@@ -32,7 +32,7 @@ public class GameControl : MonoBehaviour
         if(playerMoney>=moneyThreshold)//if the player has enough of something they win
         {
             winText.SetActive(true);
-            //LevelUnlock.unlockLevel(LvlValue);
+            LevelUnlock.unlockLvl(LvlValue);
          SceneManager.LoadScene("VictoryScreenTemp");
         }
     }
